d121462ee22f3356d98034c1dd90f2a665cb821b
asterlee0315 <asterlee0315@naver.com>
2024-08-21 21:26:07 +0900

    0821

--- SWEA 2805. 농작물 수확하기/src/Solution.java
+++ SWEA 2805. 농작물 수확하기/src/Solution.java
@@ -1,33 +1,42 @@
+import java.util.Arrays;
 import java.util.Scanner;
-
 public class Solution {
 public static void main(String[] args) {
-	Scanner sc = new Scanner (System.in);
+	Scanner sc = new Scanner(System.in);
 	int T = sc.nextInt();
 	for(int t=1; t<=T;t++) {
 		int N = sc.nextInt();
 		int [][] arr = new int [N][N];
 		
 		for(int r=0; r<N;r++) {
+			String [] a=sc.next().split("");
 			for(int c=0;c<N;c++) {
-				arr[r][c] = sc.nextInt();
-			}
-		// 반복문을 N만큼 돌면서
-
-		int [] arr2 = new int [N/2];
-		for(int i=0; i<N/2;i++) {
-			arr2[i]++;
-			}
-		int ans =0;
-		for(int j=0;j<N;j++) {
-			ans+=arr[j][N/2];
-			for(int p=1;p<=j%(N/2)+1;p++) {
-				ans+=arr[j][N/2+j%(N/2)];
-				ans+=arr[j][N/2-arr2[j%(N/2)]];
+				arr[r][c]=Integer.parseInt(a[c]);
 			}
 		}
-		System.out.println(ans);
+		// 반복문을 N만큼 돌면서 - 순서가 자기자신 자기+1 자기 +2  / 
+			// 2 -> 1~3 -> 0~4 / 1~3 / 2
+			int ans =0;
+			int cnt=0;
+				for(int i=0;i<=N/2;i++) {
+					ans+=arr[i][N/2];
+					for(int j=1;j<i+1;j++) {
+						ans+=arr[i][N/2+j];
+						ans+=arr[i][N/2-j];
+					}
+				}
+			
+			
+			for(int i=N/2+1;i<N;i++) {
+				ans+=arr[i][N/2];
+				for(int j=N-i-1;j>0;j--) {
+					ans+=arr[i][N/2+j];
+					ans+=arr[i][N/2-j];
+				}
+			}
+			
+	System.out.println("#"+t+" "+ans);
 		}
 	}
 }
-}
+
